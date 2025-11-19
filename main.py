ce3f5970cb63dd12a60d543c59b8934e1e35f095
yeondi2513 <has_25081@hana.hs.kr>
2025-11-19 11:04:09 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.tltle("첫 웹앱 입니다 !")
+st.title("첫 웹앱 입니다 !")
 name = st.text_input("이름을 입력해 주세요! : ")
 menu = st.selectbox("좋아하는 맛을 선택해주세요: ", ["아몬드봉봉", "엄마는 외계인"])
 if st.button("문장 생성"):     
